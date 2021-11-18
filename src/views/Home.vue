5760f061639a17f375ac67c3883f21def6b59d94
Jesutooni <edeworuyoyou@gmail.com>
2021-11-18 17:06:19 +0100

    background image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home"></div>
+  <div class="image bg-cover bg-bottom h-screen"></div>
 </template>
 
 <script>
@@ -7,3 +7,19 @@ export default {
   name: "Home",
 };
 </script>
+
+<style scoped>
+.image {
+  background-image: url("../assets/home/background-home-mobile.jpg");
+}
+@media (min-width: 640px) {
+  .image {
+    background-image: url("../assets/home/background-home-tablet.jpg");
+  }
+}
+@media (min-width: 1024px) {
+  .image {
+    background-image: url("../assets/home/background-home-desktop.jpg");
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          blue: {
+            dark: "#0B0D17",
+            light: "#D0D6F9",
+          },
+        },
+      },
+      fontFamily: {
+        barlow: ["'Barlow'", "sans-serif"],
+        "barlow-condensed": ["'Barlow Condensed'", "sans-serif"],
+        bellefair: ["'Bellefair'", "serif"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
